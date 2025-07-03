6e39b485b3c96fc728426b6d89124776b5e675b9
Hareshkumar Patel <hareshpatel080503@gmail.com>
2025-07-03 01:34:34 +0000

    Add update and delete methods in ReminderModel

--- app/models/ReminderModel.php
+++ app/models/ReminderModel.php
@@ -12,4 +12,17 @@ class ReminderModel {
         $stmt = $db->prepare("INSERT INTO reminders (user_id, subject, completed) VALUES (?, ?, ?)");
         $stmt->execute([$user_id, $subject, $completed]);
     }
+
+    public function delete($id, $user_id) {
+        $db = db_connect();
+        $stmt = $db->prepare("DELETE FROM reminders WHERE id = ? AND user_id = ?");
+        $stmt->execute([$id, $user_id]);
+    }
+
+    // Add completed param
+    public function update($id, $subject, $completed, $user_id) {
+        $db = db_connect();
+        $stmt = $db->prepare("UPDATE reminders SET subject = ?, completed = ? WHERE id = ? AND user_id = ?");
+        $stmt->execute([$subject, $completed, $id, $user_id]);
+    }
 }
