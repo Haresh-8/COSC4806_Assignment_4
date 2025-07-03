4dafcb663d6814658eb5fffde3a14b7075665c09
Hareshkumar Patel <hareshpatel080503@gmail.com>
2025-07-03 00:34:09 +0000

    Initial commit

---
+++ app/views/welcome/index.php
@@ -0,0 +1,8 @@
+<?php require_once 'app/views/templates/headerPublic.php'; ?>
+<div class="container text-center mt-5">
+    <h1>Welcome to COSC4806 App</h1>
+    <p class="lead">Choose an option:</p>
+    <a class="btn btn-primary m-2" href="/login">Existing User? Login</a>
+    <a class="btn btn-success m-2" href="/create">New User? Register</a>
+</div>
+<?php require_once 'app/views/templates/footer.php'; ?>
